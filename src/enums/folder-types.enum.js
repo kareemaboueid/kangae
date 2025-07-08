63272f421140b7581744f7e7bb1dae3c112ba990
kareemaboueid <kareem_aboueid@outlook.com>
2025-07-09 01:38:08 +0300

    Add folder types enum for categorizing folders in the application

---
+++ src/enums/folder-types.enum.js
@@ -0,0 +1,14 @@
+/**
+ * Folder Types Enum
+ * @enum {string}
+ */
+const enum_folder_types = {
+  home: 'home',
+  archive: 'archive',
+  trash: 'trash',
+  favorite: 'favorite',
+};
+
+export default Object.freeze(enum_folder_types);
+
+
