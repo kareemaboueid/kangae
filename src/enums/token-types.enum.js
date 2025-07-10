f0475e685e28dbec561abfce045332cc11b79eb4
kareemaboueid <kareem_aboueid@outlook.com>
2025-07-10 17:10:26 +0300

    Add JWT strategy configuration and token types enum for authentication

---
+++ src/enums/token-types.enum.js
@@ -0,0 +1,11 @@
+/**
+ * Environment Token Types Enum
+ * @description This enum defines the different types of tokens used in the application.
+ * @enum {string}
+ */
+const enum_token_types = {
+  access: 'access',
+  refresh: 'refresh'
+};
+
+export default enum_token_types;
