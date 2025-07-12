8267fa04131b7cc6a6f0d2728a3597077e151e65
kareemaboueid <kareem_aboueid@outlook.com>
2025-07-13 01:37:41 +0300

    Add token verification middleware to extract and validate bearer tokens from request headers

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ import mdwr_rate_limiter from './middlewares/rate-limiter.mdwr.js';
 import mdwr_error_converter from './middlewares/error-converter.mdwr.js';
 import mdwr_error_handler from './middlewares/error-handler.mdwr.js';
 import Utl_Api_Error from './utilities/Api-error.utl.js';
+import verifyShortToken from './configs/jwt.cnf.js';
 
 import route_v1 from './routes/v1/index-v1.js';
 
@@ -43,8 +44,23 @@ kangae_app.use(compression());
 kangae_app.use(cors());
 kangae_app.options('*', cors());
 
+// initialize getting token from request header auth as bearer token:
+kangae_app.use((req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    const token = req.headers.authorization.split(' ')[1];
+    try {
+      req.tokenPayload = verifyShortToken(token);
+    } catch (error) {
+      return res.status(error.statusCode || 500).json({ message: error.message });
+    }
+  } else {
+    req.tokenPayload = null;
+  }
+  next();
+});
 
-// limit repeated failed requests to auth endpoints in production:
+
+// apply rate limiting to auth endpoints on production:
 if (cnf_env_variables.env_mode === 'production') {
   kangae_app.use('/v1/auth', mdwr_rate_limiter);
 }
