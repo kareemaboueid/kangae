5eb3c2cd30253783dde7dd3c1a976c0fe6aaa4f7
kareemaboueid <kareem_aboueid@outlook.com>
2025-07-13 01:38:32 +0300

    Refactor API routes to remove authentication route and include user route for version 1

--- src/routes/v1/index-v1.js
+++ src/routes/v1/index-v1.js
@@ -2,17 +2,18 @@ import express from 'express';
 import utl_routes_mapping from '../../utilities/routes-mapping.utl.js';
 
 // Importing routes
-import route_auth from './auth.route.js';
+import route_user from './user.route.js';
 
 /**
- * ### App Route
+ * ### App Route version 1
+ * @description This module defines the main routes for version 1 of the API.
  * @version 1
  * @endpoint `/v1`
  */
 const route_v1 = express.Router();
 
 const v1_main_routes = [
-  { name: 'route_auth', path: '/auth', route: route_auth },
+  { name: 'route_user', path: '/users', route: route_user },
 ];
 
 // Map routes to router:
